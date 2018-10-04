134260f408aba4c1af0bddfa132d201ea44ebecc
Mr <lkarpik@wp.pl>
2018-10-04 09:36:52 +0200

    Added functionality add question, project finished

--- public/add.php
+++ public/add.php
@@ -1,9 +1,56 @@
 <?php
     include 'db.php';
+    session_start();
 
     if (isset($_POST['submit'])) {
+        $number = $_POST['question_number'];
+        $text = $_POST['question_text'];
+        $correct_choice = $_POST['correct_choice'];
+        // Choicess array
+        $choices = array();
+        $choices[1] = $_POST['choice1']; 
+        $choices[2] = $_POST['choice2']; 
+        $choices[3] = $_POST['choice3']; 
+        $choices[4] = $_POST['choice4']; 
+        $choices[5] = $_POST['choice5']; 
+        // $choices[0] = $_POST['correct_choice']; 
         
+        // add question query
+        $query = "INSERT INTO questions (question_number, text)
+                    VALUES ('$number', '$text')";
+        
+        // Run query
+        $insert = $mysqli->query($query) or die($mysqli->error.__LINE__);
 
+        // validate 1st insert
+        if ($insert) {
+            foreach ($choices as $choice => $value) {
+                if ($value != '') {
+                    if ($choice == $correct_choice) {
+                        $is_correct = 1;
+                    } else {
+                    $is_correct = 0;
+                    }
+                    // choice query
+                    $query = "INSERT INTO choices (question_number, text, corect)
+                                VALUES ('$number', '$value', '$is_correct')";
+                    // Run
+                    $insert = $mysqli->query($query) or die($mysqli->error.__LINE__);
+
+                    if ($insert) {
+                        continue;
+                    } else {
+                        die('Error : '.$mysqli->error);
+                    }
+
+                } 
+
+            }
+            $msg = 'Question added';
+        }
+    
+    
+    
     }
 
 ?>
@@ -27,10 +74,16 @@
     <main>
         <div class="container">
             <h2>Add a question</h2>
+            <?php
+                if (isset($msg)) {
+                    echo '<p>'.$msg.'</p>';
+                }
+            
+            ?>
             <form action="add.php" method="post">
                 <p>
                     <label for="question_number">Question number:</label>
-                    <input type="number" name="question_number">
+                    <input type="number" name="question_number" value='<?= $_SESSION['total']+1 ?>'>
                 </p>
                 <p>
                     <label for="question_text">Question text:</label>
